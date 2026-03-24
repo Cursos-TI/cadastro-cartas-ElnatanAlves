beb76df1f9361fb2079eb59aecd837dc9db01796
Elnatan Alves <elnatanalves@outlook.com>
2026-03-24 19:57:25 -0300

    primeira etapa do jogo criado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@ char nomeDoJogador [50];
 int main (){
 
 //  Var. Cartas 1  
-char IncialDoEstado;
+char incialDoEstado;
 char codigoCarta [20];
 char nomeDaCidade [50];
 int quantidadePopulacao;
@@ -17,7 +17,7 @@ float PIB;
 int qtdPontosTuristicos;
 
 // Var. Cartas 2
-char IncialDoEstado2;
+char incialDoEstado2;
 char codigoCarta2 [20];
 char nomeDaCidade2 [50];
 int quantidadePopulacao2;
@@ -33,5 +33,41 @@ printf("Para iniciar o jogo vamos precisar que você envie "
 
 printf("\nInicialmente preciso que você se identifique: ");
 scanf("%s", nomeDoJogador);
+
+printf("\nPerfeito, %s agora digite apenas a inicial do estado (1 letra):", nomeDoJogador);
+scanf(" %c", &incialDoEstado);
+
+printf("\nAgora, digite o código da carta que você irá lançar. Ex. A01, A02...: ");
+scanf("%s", codigoCarta);
+getchar();
+
+printf("\nE qual o nome da cidade dentro do estado que você quer jogar? ");
+fgets(nomeDaCidade, 50, stdin);
+nomeDaCidade[strcspn(nomeDaCidade, "\n")] = '\0';
+
+printf("\n%s, você sabe o numero aproximado da população da sua cidade? ", nomeDoJogador);
+scanf("%d", &quantidadePopulacao);
+
+printf("\nEntendido, existe aproximadamente, %d isto é ótimo, agora me fala a área em Km² "
+"precisa só digitar o numero: ");
+scanf("%f", &areaEmKm);
+
+printf("\nEstamos indo bem, agora me informa o PIB aproximado da sua cidade: ");
+scanf("%f", &PIB);
+
+printf("\nUau, fiquei com surpreso com a ultima informação, provavelmente a cidade deve ter "
+"òtimos pontos turisticos, sabe me informar quantos? ");
+scanf("%d", &qtdPontosTuristicos);
+
+printf("\n=== Resumo da Carta 1 Lançada por %s ===\n", nomeDoJogador);
+printf("Estado: %c\n", incialDoEstado);
+printf("Código da Carta: %s\n", codigoCarta);
+printf("Nome da Cidade: %s\n", nomeDaCidade);
+printf("População: %d\n", quantidadePopulacao);
+printf("Área: %.2f Km² \n", areaEmKm);
+printf("PIB: %.2f bilhões de reais \n", PIB);
+printf("Número de Pontos Turisticos: %d\n", qtdPontosTuristicos);
+printf("\n=== %s Aguarde o outro jogador lançar a carte dele ===\n", nomeDoJogador);
+
 
 }
